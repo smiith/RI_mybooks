fbb72433ab1cc52d2844d41e7f93e4f2eac3d6ec
Sergej Karavajev <job@vprage.info>
2019-01-26 09:54:45 +0100

    Refactored Book and Bookshelf

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -2,32 +2,34 @@ import React from 'react';
 import _ from 'lodash';
 
 
-const renderAuthor = (author,i) => (<div key={i} className="book-authors">{author}</div>);
+const renderAuthor = (author, i) => (<div key={i} className="book-authors">{author}</div>);
 
 export default ({book, handleOnChange}) => (
-    <div className="book">
-        <div className="book-top">
-            <div
-                className="book-cover"
-                style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage: `url(${_.get(book, 'imageLinks.smallThumbnail')})`,
-                }}
-            />
-            <div className="book-shelf-changer">
-                <select onChange={e => handleOnChange(book, e.target.value)} value={book.shelf}>
-                    <option value="move" disabled>
-                        Move to...
-                    </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                </select>
+    <li>
+        <div className="book">
+            <div className="book-top">
+                <div
+                    className="book-cover"
+                    style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: `url(${_.get(book, 'imageLinks.smallThumbnail')})`,
+                    }}
+                />
+                <div className="book-shelf-changer">
+                    <select onChange={e => handleOnChange(book, e.target.value)} value={book.shelf}>
+                        <option value="move" disabled>
+                            Move to...
+                        </option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
+                </div>
             </div>
+            <div className="book-title">{book.title}</div>
+            {book.authors.map(renderAuthor)}
         </div>
-        <div className="book-title">{book.title}</div>
-        {book.authors.map(renderAuthor)}
-    </div>
+    </li>
 );
--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import Book from '../Book/Book';
 
-
-const renderBook = (book, handleOnChange) => (
-    <li key={book.id} >
-        <Book book={book} handleOnChange={handleOnChange} />
-    </li>
-);
-
 export default ({title, books, handleOnChange}) => (
     <div className="bookshelf">
         <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
             <ol className="books-grid">
-                {books.map(book => renderBook(book, handleOnChange))}
+                {books.map(book => <Book key={book.id} book={book} handleOnChange={handleOnChange} />)}
             </ol>
         </div>
     </div>
